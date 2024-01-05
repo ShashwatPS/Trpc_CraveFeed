974b092dcbd1b491867cab8d927ad8b93ddf808f
Shashwat Singh <shashwat123student@gmail.com>
2024-01-05 23:32:16 +0530

    Newpost working

--- cravefeed/src/app/page.tsx
+++ cravefeed/src/app/page.tsx
@@ -4,15 +4,21 @@ import { trpc } from './_trpc/client';
 import {z} from "zod";
 
 export default function IndexPage() {
-  const mutation = trpc.getfollowing.useMutation();
+  const mutation = trpc.newPost.useMutation();
   const signup = () => {
-    mutation.mutate({
-        Id: 1,
-    })
-      const responseData = mutation.data;
-      console.log("Mutation Data: ",responseData);
-  }
-  return(
+        mutation.mutate({
+            userId: 1,
+            restaurantName: "Metro",
+            cityName: "Guna",
+            dishName: "Butter Chicken",
+            caption: "Was a super great dish no cap",
+            image: "fsdfdsfsd",
+            location: "dasdasdasdasdsadsad",
+        });
+        // const responseData = mutation.data;
+        // console.log("Mutation Data: ", responseData);
+    }
+    return(
       <div>
         <h1>Button for checking</h1>
         <button onClick={signup} disabled={mutation.isLoading}>
