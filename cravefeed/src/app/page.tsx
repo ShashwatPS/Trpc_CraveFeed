a4d83b78b70f2d8b8fc87bb003e245fdfabbf25b
Shashwat Singh <shashwat123student@gmail.com>
2024-01-06 17:39:38 +0530

    Testing Done

--- cravefeed/src/app/page.tsx
+++ cravefeed/src/app/page.tsx
@@ -1,14 +1,36 @@
 "use client";
 
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { authOptions } from '@/app/lib/auth';
 
 export default function Home() {
+    const { data: session } = useSession();
+
+    async function handleSignIn() {
+        const result = await signIn('github', { callbackUrl: '/test' });
+        if (result?.error) {
+            console.error('Sign-in failed:', result.error);
+        } else {
+            console.log('Sign-in successful!');
+            console.log('Session details:', session);
+
+        }
+    }
     return (
-        <button
-            onClick={() => signIn("github")}
-            className="flex items-center justify-center gap-2 rounded-lg bg-teal-500 px-8 py-3 text-center text-sm font-semibold text-white ring-teal-300 transition duration-100 hover:bg-teal-600 md:text-base"
-        >
-            Login
-        </button>
+        <div>
+            <button
+                onClick={handleSignIn}
+                className="flex items-center justify-center gap-2 rounded-lg bg-teal-500 px-8 py-3 text-center text-sm font-semibold text-white ring-teal-300 transition duration-100 hover:bg-teal-600 md:text-base"
+            >
+                Login
+            </button>
+
+            {session && (
+                <div>
+                    <p>Welcome, {session.user.name}!</p>
+                </div>
+            )}
+        </div>
     );
 }
